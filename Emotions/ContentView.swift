b659239128937f1243fb429d74053cc1012fca60
Witek Bobrowski <witek@bobrowski.com.pl>
2020-05-10 16:12:19 +0200

    add initial xcode project

---
+++ Emotions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Emotions
+//
+//  Created by Witek Bobrowski on 10/05/2020.
+//  Copyright © 2020 Witek Bobrowski. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
